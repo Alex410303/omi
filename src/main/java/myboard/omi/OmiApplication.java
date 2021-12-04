be0c3b319c825219790c852d754a6930fc400302
admin <alex410303@gmail.com>
2021-12-04 12:10:58 +0200

    Initial commit

---
+++ src/main/java/myboard/omi/OmiApplication.java
@@ -0,0 +1,13 @@
+package myboard.omi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmiApplication.class, args);
+	}
+
+}
